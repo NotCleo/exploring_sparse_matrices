c4402f6c76a38f785a4549e8bb1beb6b5d86649c
Amrut <144106562+NotCleo@users.noreply.github.com>
2025-03-04 01:48:04 +0530

    Create PBSJobScriptviaCuda_appliedsharedmem.sh

---
+++ PBSJobScriptviaCuda_appliedsharedmem.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=1:gpus=1  # Request 1 CPU core, 1 GPU
+#PBS -l walltime=00:10:00      # Max runtime of 10 minutes
+#PBS -N SparseMatrixCUDAOpt    # Job name
+
+cd $PBS_O_WORKDIR
+./spmv_cuda_opt
